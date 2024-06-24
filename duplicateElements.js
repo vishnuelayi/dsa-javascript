3338ade9fee9289e314f5e4e6e1f6de4236519ef
Vishnu Elayi <vishnuelayi@gmail.com>
2024-06-24 21:36:25 +0530

    added duplicate elements and replace with it _

---
+++ duplicateElements.js
@@ -0,0 +1,19 @@
+function removeDuplicate(sortedArray) {
+  for (let i = 0; i <= sortedArray.length-1 ; i++) {
+    if (sortedArray[i] === "_") continue;
+    for (let j = i + 1; j <= sortedArray.length - 1; j++) {
+      if (sortedArray[i] === sortedArray[j]) {
+        sortedArray[j] = "_";
+      }
+    }
+  }
+
+  return sortedArray;
+}
+
+const inputArray = [2, 4, 2, 5, 2, 1, 9];
+
+const sortedArray = inputArray.sort((a, b) => a - b);
+
+result = removeDuplicate(sortedArray);
+console.log(result);
